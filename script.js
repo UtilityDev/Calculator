0ece516cd532e295a88d3ce0feefbd69f8789cee
Julian ⚡ <45518053+UtilityDev@users.noreply.github.com>
2024-03-03 21:41:49 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+let firstNumber = null;
+let operator = null;
+let secondNumber = null;
+
+function add () {
+    
+}
+
+function subtract () {
+
+}
+
+function multiply () {
+
+}
+
+function divide () {
+
+}
